55e7289085056c5fc8b990a3fd8dcf1111d1af57
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-12-09 19:38:48 +0100

    Initial and test IngredientDao

---
+++ src/main/java/com/example/recipe_jpa/data/DAO/IngredientDao.java
@@ -0,0 +1,13 @@
+package com.example.recipe_jpa.data.DAO;
+
+import com.example.recipe_jpa.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IngredientDao extends JpaRepository<Ingredient,Integer> {
+
+    Ingredient findByIngredientNameIgnoringCase(String name);
+
+    List<Ingredient> findAllByIngredientNameContainingIgnoringCase(String name);
+}
---
+++ src/test/java/com/example/recipe_jpa/data/DAO/IngredientDaoTest.java
@@ -0,0 +1,57 @@
+package com.example.recipe_jpa.data.DAO;
+
+import com.example.recipe_jpa.model.Ingredient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class IngredientDaoTest {
+
+    @Autowired
+    private IngredientDao testObject;
+
+    @Autowired
+    private TestEntityManager em;
+
+
+
+
+    @Test
+    void findByIngredientNameIgnoringCase() {
+        Ingredient ingredient = em.persist(new Ingredient("Salt"));
+
+        Ingredient result = testObject.findByIngredientNameIgnoringCase("salt");
+
+        assertNotNull(result);
+
+        assertEquals(ingredient,result);
+    }
+
+    @Test
+    void findAllByIngredientNameContainingIgnoringCase(){
+        List<Ingredient> ingredients = new ArrayList<>(
+                Arrays.asList(
+                        new Ingredient("Potato"),
+                        new Ingredient("Tomato"),
+                        new Ingredient("Salt"),
+                        new Ingredient("Milk")
+                )
+        );
+
+        ingredients.forEach(em::persist);
+
+        List<Ingredient> result = testObject.findAllByIngredientNameContainingIgnoringCase("Ato");
+
+        int expected = 2;
+
+        assertEquals(2,result.size());
+    }
+}
